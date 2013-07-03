d35b64fdcbd822b6f140f13fc16e36428b1dd5b7
帅印 <shuaiyinoo@163.com>
2013-07-03 16:52:40 +0800

    加入版本信息
    给营运微视加入版本信息

--- YYWS/ViewController.m
+++ YYWS/ViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "ViewController.h"
+#import "DateSynViewController.h"
+#import "SystemSetViewController.h"
+#import "VersionInfoViewController.h"
 
 @interface ViewController ()
 
@@ -43,14 +46,23 @@
 }
 //定义界面按钮数据同步
 -(IBAction)dateSynButtonOnClick:(id)Sender{
+    DateSynViewController *datesyn = [[DateSynViewController alloc]initWithNibName:@"DateSynViewController" bundle:nil];
+    datesyn.title = @"数据同步";
     
+    [self.navigationController pushViewController:datesyn animated:true];
 }
 //定义界面按钮系统设置
 -(IBAction)systemSetButtonOnClick:(id)Sender{
+    SystemSetViewController *systemSet = [[SystemSetViewController alloc]initWithNibName:@"SystemSetViewController" bundle:nil];
+    systemSet.title = @"系统设置";
     
+    [self.navigationController pushViewController:systemSet animated:true];
 }
 //定义界面按钮版本信息
 -(IBAction)versionInfoButtonOnClick:(id)Sender{
+    VersionInfoViewController *verion = [[VersionInfoViewController alloc]initWithNibName:@"VersionInfoViewController" bundle:nil];
+    verion.title = @"版本信息";
     
+    [self.navigationController pushViewController:verion animated:true];
 }
 @end
